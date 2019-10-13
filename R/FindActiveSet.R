24115b8a1134e52770b90fc1a9c80bc74d3a6135
Nicholas Henderson <nchender@sph-190157-bio.ddns.sph.umich.edu>
2019-10-12 21:17:33 -0400

    initial commit

---
+++ R/FindActiveSet.R
@@ -0,0 +1,17 @@
+FindActiveSet <- function(theta, utimes0, utimes1) {
+    ## This for the case when control treatment dominates active treatment early on
+    ## In this case, we need to have w_{0j} = 0 for j such that t_{0j} \leq t_{11}
+    ## We also want to ignore w_{1j} such that \theta < w_{1j} < \min\{t_{0j}: t_{0j} > theta}
+  
+    ind0 <- utimes0 <= utimes1[1]
+    ## w0[ind0] = 0 
+    
+    first.pass.theta <- min(utimes0[utimes0 > theta]) # make this more robust later
+    ind1 <- utimes1 > theta & utimes1 <= first.pass.theta
+  
+    ans <- list(active.set0 = !ind0, active.set1=!ind1)
+    return(ans)
+}
+
+
+
