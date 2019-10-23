74a774265e55241c44effa689514515e80cab6f8
Nicholas Henderson <nchender@sph-190157-bio.ddns.sph.umich.edu>
2019-10-23 18:23:33 -0400

    added dots argument

--- R/plotsurvdelay.R
+++ R/plotsurvdelay.R
@@ -1,8 +1,8 @@
-plot.surv.delay <- function(obj, type="surv") {
+plot.surv.delay <- function(obj, type="surv", ...) {
   ## plotting function
   if(type=="surv") {
     plot(obj$times0, obj$surv0, type="n", las=1, xlab="Time", ylab="Survival Prob.",
-         xlim=c(0, max(c(obj$times0, obj$times1))), ylim=c(0,1))
+         xlim=c(0, max(c(obj$times0, obj$times1))), ylim=c(0,1), ...)
     lines(c(0,obj$times0), c(1, obj$surv0), type="s", lwd=2)
     lines(c(0,obj$times1), c(1, obj$surv1), type="s", col="red", lwd=2)
     abline(v=obj$theta, lwd=2)
