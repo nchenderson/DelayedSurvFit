76dd820c54e25748782b07a135d7a4a9756bfad3
Nicholas Henderson <nchender@sph-190157-bio.ddns.sph.umich.edu>
2019-11-26 14:06:10 -0500

    modified inequality issue

--- R/FindActiveSet.R
+++ R/FindActiveSet.R
@@ -6,14 +6,25 @@ FindActiveSet <- function(theta, gamma, utimes0, utimes1) {
     if(gamma==1) {
         ind0 <- utimes0 <= utimes1[1]
         ## w0[ind0] = 0 
-    
-        first.pass.theta <- min(utimes0[utimes0 > theta]) # make this more robust later
+        tmp.ind <- utimes0 > theta
+        if(sum(tmp.ind) > 0) {
+             first.pass.theta <- min(utimes0[utimes0 > theta]) # make this more robust later
+        } else {
+             first.pass.theta <- Inf
+        }
         ind1 <- utimes1 > theta & utimes1 <= first.pass.theta
   
         ans <- list(active.set0 = !ind0, active.set1=!ind1)
     } else if(gamma == -1) {
         ind1 <- utimes1 <= utimes0[1]
-        first.pass.theta <- min(utimes1[utimes1 > theta])
+        
+        tmp.ind <- utimes1 > theta
+        if(sum(tmp.ind) > 0) {
+          first.pass.theta <- min(utimes1[utimes1 > theta]) # make this more robust later
+        } else {
+          first.pass.theta <- Inf
+        }
+       # first.pass.theta <- min(utimes1[utimes1 > theta])
         ind0 <- utimes0 > theta & utimes0 <= first.pass.theta
         
         ans <- list(active.set0 = !ind0, active.set1=!ind1)
