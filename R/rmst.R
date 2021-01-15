713178e2a7791db8221309ac6b90f3b256356fde
Nicholas Henderson <nchender@NicholassiMac2.attlocal.net>
2021-01-15 16:15:22 -0500

    removed unnecessary comments

--- R/rmst.R
+++ R/rmst.R
@@ -17,22 +17,6 @@ rmst <- function(obj, tau0=NULL, tau1=NULL)
   rmst0 <- sum(S0*diff(t0))
   rmst1 <- sum(S1*diff(t1))
   
-  
-  #areas0 <- time.diff0*c(1, trun.surv0)
-  
-  #trunc.time0 <- sort(c(obj$times0[ind0], tau0))
-  #trun.surv0 <- obj$surv0[ind0]
-  #time.diff0 <- diff(c(0, trunc.time0))
-  #areas0 <- time.diff0*c(1, trun.surv0)
-  
-  #ind1 <- obj$times1 <= tau1
-  #trunc.time1 <- sort(c(obj$times1[ind1], tau1))
-  #trun.surv1 <- obj$surv1[ind1]
-  #time.diff1 <- diff(c(0, trunc.time1))
-  #areas1 <- time.diff1*c(1, trun.surv1)
-  
-  #rmst0 <- sum(areas0)
-  #rmst1 <- sum(areas1)
   return(list(rmst0=rmst0, rmst1=rmst1))
 }
 
