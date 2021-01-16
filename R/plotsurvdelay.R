da3ced07ae2d2b9dc9dcc51703f1b21f902dbb88
Nicholas Henderson <nchender@NicholassiMac2.attlocal.net>
2021-01-15 23:57:57 -0500

    changed plot method to allow for plotting hazards

--- R/plotsurvdelay.R
+++ R/plotsurvdelay.R
@@ -1,20 +1,21 @@
-plot.surv.delay <- function(obj, type="surv", ...) {
+plot.surv.delay <- function(x, ..., type="surv") {
   ## plotting function
   if(type=="surv") {
-    plot(obj$times, obj$surv0, type="n", las=1, xlab="Time", ylab="Survival Prob.",
-         xlim=c(0, max(obj$times)), ylim=c(0,1), ...)
-    lines(c(0,obj$times), c(1, obj$surv0), type="s", lwd=2)
-    lines(c(0,obj$times), c(1, obj$surv1), type="s", col="red", lwd=2)
-    abline(v=obj$theta, lwd=2)
-    legend("bottomleft", legend=c("Control Arm", "Active Trt. Arm"), col=c("black", "red"), lwd=3,
+     plot(c(0, rep(x$times, 2)), c(1,x$surv0, x$surv1), type="n", ...)
+     lines(c(0, x$times), c(1, x$surv0), type="s", lwd=2)
+     lines(c(0, x$times), c(1, x$surv1), type="s", col="red", lwd=2)
+     abline(v=x$theta, lwd=2)
+     legend("bottomleft", legend=c("Control Arm", "Active Trt. Arm"), col=c("black", "red"), lwd=3,
            bty='n')
-  } else if(type=="cum.hazard") {
-    plot(obj$times0, cumsum(obj$hazard0), type="n", las=1, xlab="Time", ylab="Cumulative Hazard",
-         xlim=c(0, max(c(obj$times0, obj$times1))))
-    lines(obj$times0, cumsum(obj$hazard0), type="s", lwd=2)
-    lines(obj$times1, cumsum(obj$hazard1), type="s", col="red", lwd=2)
-    abline(v=obj$theta, lwd=2)
-    legend("topleft", legend=c("Control Arm", "Active Trt. Arm"), col=c("black", "red"), lwd=3,
-           bty='n')
+     ans <- data.frame(x=rep(x$times, 2), y=c(x$surv0, x$surv1), trt=rep(c(0,1), each=length(x$times)))
+  } else if(type=="hazard") {
+     plot(rep(x$times, 2), c(x$haz0, x$haz1), type="n", ...)
+     lines(x$times, x$haz0, type="s", lwd=2)
+     lines(x$times, x$haz1, type="s", col="red", lwd=2)
+     abline(v=x$theta, lwd=2)
+     legend("topleft", legend=c("Control Arm", "Active Trt. Arm"), col=c("black", "red"), lwd=3,
+            bty='n')
+     ans <- data.frame(x=rep(x$times, 2), y=c(x$haz0, x$haz1), trt=rep(c(0,1), each=length(x$times)))
   }
+  return(ans)
 }
